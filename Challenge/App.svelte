97ef976d0d31d48c02d780f18e47943b804d186a
frontendella <ryukhinaella@gmail.com>
2023-02-20 18:13:55 -0800

    index in Face.svelte

---
+++ Challenge/App.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Face from './Face.svelte';
+    let say = 'hi';
+    setTimeout(() => {
+        say = 'bye';
+    }, 1000)
+</script>
+
+<style>
+    div {
+        color: red;
+    }
+</style>
+
+<div>
+    Say: {say}
+</div>
+
+<Face index={0} />
+<Face />
+<Face index={2}/>
+
+<!-- 
+1. add a prop called index in Face.svelte
+2. give it a default value
+3. add a list of faces in Face.svelte
+4. use the index in the list to choose the face
+5. add three faces onto your page.
+-->
