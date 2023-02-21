f498e3979906b80f49b85358e6c69f002bbf17f2
frontendella <ryukhinaella@gmail.com>
2023-02-20 17:56:09 -0800

    Importing and exporting

---
+++ Importing and exporting/App.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Face from './Face.svelte';
+    let say = 'hi';
+    setTimeout(() => {
+        say = 'bye';
+    }, 1000)
+</script>
+
+<style>
+    div {
+        color: red;
+    }
+</style>
+
+<div>
+    Say: {say}
+</div>
+
+<Face size="4" />
+<Face size="10" />
+<Face />
