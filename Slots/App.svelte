b433c099ce5eac617e2cd912d6ba9a642a057fba
frontendella <ryukhinaella@gmail.com>
2023-02-20 18:23:33 -0800

    Slots

---
+++ Slots/App.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Face from './Face.svelte';
+    import Container from './Container.svelte';
+    let say = 'hi';
+    setTimeout(() => {
+        say = 'bye';
+    }, 1000)
+</script>
+
+<Container>
+    <div>
+        Say: {say}
+    </div>
+
+    <Face index={0} />
+    <Face />
+    <Face index={2}/>
+</Container>
+
+
+
+<style>
+    div {
+        color: red;
+    }
+    :global(*) {
+		box-sizing: border-box;
+	}
+	:global(body, html) {
+		margin: 0;
+		height: 100vh;
+		overflow: hidden;
+	}
+</style>
