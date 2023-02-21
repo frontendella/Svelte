8f87cfb8508901e139a4bfe7c99fcc05793020b3
frontendella <ryukhinaella@gmail.com>
2023-02-20 19:02:40 -0800

    Event dispatching

---
+++ Event dispatching/App.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Face from './Face.svelte';
+    import Container from './Container.svelte';
+    import Header from './Header.svelte';
+    import Buttons from './Buttons.svelte';
+    let showHeader = false;
+</script>
+
+
+{#if showHeader}
+    <Header />
+{/if}
+<Container>
+    <Buttons on:click={(e) => {showHeader = e.detail}} />
+</Container>
+
+<style>
+    div {
+        color: red;
+    }
+    :global(*) {
+		box-sizing: border-box;
+	}
+	:global(body, html) {
+		margin: 0;
+		height: 100vh;
+		overflow: hidden;
+	}
+</style>
---
+++ Event dispatching/Buttons.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {createEventDispatcher} from 'svelte';
+    const dispatch = createEventDispatcher();    
+</script>
+
+<button on:click={() => dispatch('click', true)}>
+    Show
+</button>
+<button on:click={() => dispatch('click', false)}>
+    Hide
+</button>
+
+<style>
+    button {
+        font-family: 'Nunito', sans-serif;
+		display: block;
+        font-size: 1.2rem;
+        margin: 10px;
+        transition: 1s;
+        padding: 0.7em 1em;
+        cursor: pointer;
+        border-radius: 50px;
+        background-color: #40b3ff;
+        color: white;
+        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+	}
+</style>
